9019d4e15620419d27077595c4c6160a7d0bbf90
张新征 <programer.email@gmail.com>
2019-01-14 22:45:13 +0800

    init project

---
+++ Main.js
@@ -0,0 +1,14 @@
+import {ResourceLoader} from './js/base/ResourceLoader.js'
+const resourceLoader = new ResourceLoader()
+export class Main {
+  constructor () {
+    this.canvas = document.getElementById("game_canvas")
+    this.ctx = this.canvas.getContext("2d")
+    const loader = ResourceLoader.create()
+    loader.onLoaded(map => this.onResourceFirstLoaded(map))
+  }
+
+  onResourceFirstLoaded (map) {
+    console.log(map)
+  }
+}
